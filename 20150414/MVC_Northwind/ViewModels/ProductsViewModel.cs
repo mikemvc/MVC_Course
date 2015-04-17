51a85cbc28be27e8cea1609f434ebf6b14c41d86
mikemvc <mikemvc@hotmail.com>
2015-04-17 08:44:19 +0800

    新增ProductsViewModel

---
+++ 20150414/MVC_Northwind/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Northwind.ViewModels
+{
+    // 新增的ProductsViewModel
+    public class ProductsViewModel
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public Nullable<int> SupplierID { get; set; }
+        public Nullable<int> CategoryID { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public Nullable<decimal> UnitPrice { get; set; }
+        public Nullable<short> UnitsInStock { get; set; }
+        public Nullable<short> UnitsOnOrder { get; set; }
+        public Nullable<short> ReorderLevel { get; set; }
+        public bool Discontinued { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
